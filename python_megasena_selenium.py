467cf8c4061f590b2b82510bd367f164a0f63e3d
Thiago ERS <thiago.ers@gmail.com>
2023-01-02 19:14:08 -0300

    Acrescentei exception handlers e reduzi tempos

--- python_megasena_selenium.py
+++ python_megasena_selenium.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
-import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
+import time
 import math
 import queue
 import multiprocessing
@@ -14,97 +14,136 @@ def get_table_len():
     start_time=datetime.now().strftime("%H:%M:%S")
     print('start time:',start_time)
     load_failed=False
-    tentativas=5
-    # Configura o webdriver.chrome em uma variável chamada browser
+    tentativas=15
+
 
     try:
         browser = webdriver.Chrome("C:\Program Files (x86)\chromedriver.exe")
     except Exception as e:
         print(e)
-    
-    # Este método define um tempo limite fixo para esperar 
-    # implicitamente que um elemento seja encontrado ou que um comando seja concluído.
-    browser.implicitly_wait(10)
-
-
-    # Acessa a página da megasena
 
     MEGASENA_URL = "https://loterias.caixa.gov.br/Paginas/Mega-Sena.aspx" 
     browser.get(MEGASENA_URL)
 
-    # Maximiza a página atualmente aberta
-    browser.maximize_window()
-
-    # Este trecho espera 10 segundos para detectar se um elemento da tela já se encontra visível
-    # Caso esteja, indica indiretamente que a tela foi carregada;senão, lança uma excessão e aguarda mais 10 segundos.
     timeout = 10 
     try:
         element_present = EC.presence_of_element_located((By.ID, 'PlaceHolderMain_g_8948ce2c_ab1a_4e3c_95ec_8eb3565a1df1'))
         WebDriverWait(browser, timeout).until(element_present)
     except :
-        print("Timed out waiting for page to load - 1")
-        time.sleep(10)
-
-    # Efetua clique em "aceitar" no banner aberto sobre a página principal com objetivo de fechá-lo e permitir
-    # o controle remoto da página pelo selenium
-   
-    clicar=browser.find_element(By.XPATH,'/html/body/div[4]/div/div[2]/button[3]') 
-    time.sleep(5)
-    clicar.click()
-
-
-    # Efetua o clique no link de resultados de jogos anteriores da megasena
-    RESULTADOS_URL=browser.find_element(By.CSS_SELECTOR, "#resultados > div > ul > li > a")
-    time.sleep(3)
-    RESULTADOS_URL.click()
-    time.sleep(5)
-    # Muda a target screen para a página de resultados que acabou de ser carregada
-    browser.switch_to.window(browser.window_handles[1])
-
-    # Este trecho tenta detectar por 10 segundos um elemento da tabela de resultados;
-    # Em caso negativo, lança uma excessão e muda a variável load_failed para True.
-    timeout = 10
-    try:
-        element_present = EC.presence_of_element_located((By.ID, 'ctl50_g_cf05b8d5_fd75_46b5_bdfa_a623e654362c'))
-        WebDriverWait(browser, timeout).until(element_present)
-    except:
+        print("Timed out na tentativa de carregamento da página principal em get_table_len")
+        time.sleep(2)
         load_failed=True
 
-    # Após a excessão anterior, este loop tenta detectar por 10 vezes um elemento da tabela de resultados
-    # Para cada tentativa falha, atualiza a tela com o objetivo de que ela seja recarregada.
     while load_failed==True and tentativas > 0 :
-
+        browser.refresh()
+        time.sleep(10)
         timeout = 10
         try:
-            element_present = EC.presence_of_element_located((By.ID, 'ctl50_g_cf05b8d5_fd75_46b5_bdfa_a623e654362c'))
+            element_present = EC.presence_of_element_located((By.ID, 'PlaceHolderMain_g_8948ce2c_ab1a_4e3c_95ec_8eb3565a1df1'))
             WebDriverWait(browser, timeout).until(element_present)
             
         except:
-            browser.refresh()
-            tentativas = tentativas - 1
-        finally:
+            if tentativas > 0:
+                tentativas = tentativas - 1
+                print('Tentando novamente...')
+
+            else:
+                print('Todas as tentativas de carregamento da pagina falharam...')
+                exit()
+        else:
             load_failed=False
 
-    # Coleta os nomes das colunas da tabela
+    tentativas=15
+
+    browser.maximize_window()
+
+
+    timeout = 10 
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[2]/button[3]'))
+        WebDriverWait(browser, timeout).until(element_present)
+    except :
+        print("Botão de aceite não localizado na página principal.")
+        time.sleep(2)
+        load_failed=True
+
+    while load_failed==True and tentativas > 0 :
+        browser.refresh()
+        time.sleep(10)
+        timeout = 10
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[2]/button[3]'))
+            WebDriverWait(browser, timeout).until(element_present)
+            
+        except:
+            if tentativas > 0:
+                tentativas = tentativas - 1
+                print('Tentando novamente...')
+
+            else:
+                print('Todas as tentativas falharam...')
+                exit()
+        else:
+            load_failed=False
+
+    tentativas=15
+
+    clicar=browser.find_element(By.XPATH,'/html/body/div[4]/div/div[2]/button[3]') 
+    time.sleep(2)
+    clicar.click()
+
+
+    
+    RESULTADOS_URL=browser.find_element(By.CSS_SELECTOR, "#resultados > div > ul > li > a")
+    time.sleep(2)
+    RESULTADOS_URL.click()
+    time.sleep(2)
+
+    browser.switch_to.window(browser.window_handles[1])
+
+    timeout = 10
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="ctl50_g_cf05b8d5_fd75_46b5_bdfa_a623e654362c"]/div/div/table/thead/tr/th[1]/small'))
+        WebDriverWait(browser, timeout).until(element_present)
+    except:
+        print('Link dos resultados anteriores não encontrado.')
+        load_failed=True
+        browser.refresh()
+
+    while load_failed==True and tentativas > 0 :
+        browser.refresh()
+        time.sleep(10)
+        timeout = 10
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="ctl50_g_cf05b8d5_fd75_46b5_bdfa_a623e654362c"]/div/div/table/thead/tr/th[1]/small'))
+            WebDriverWait(browser, timeout).until(element_present)
+            
+        except:
+            if tentativas > 0:
+                tentativas = tentativas - 1
+                print('Tentando novamente...')
+
+            else:
+                print('Todas as tentativas falharam...')
+                exit()
+        else:
+            load_failed=False
+
     HEADERS = browser.find_elements(By.TAG_NAME, "th")
     time.sleep(2)
 
-    # Extrai cada nome de coluna para a variável cols
     cols = []
     for h in HEADERS:
         cols.append(h.text)
 
-    # Verifica qual é o número do último sorteio registrado na tabela de resultados para saber quantas linhas há no total.
-
     last_element = browser.find_elements(
         By.XPATH, '//*[@id="ctl50_g_cf05b8d5_fd75_46b5_bdfa_a623e654362c"]/div/div/table/tbody[last()]/tr/td[1]')
+
     time.sleep(2)
-    # Recupera o número do último sorteio.
 
     total_len = int(str(last_element[0].text).replace('[', '').replace(']', '').replace('\'', ''))
-
     time.sleep(2)
-    # Salva em arquivo de texto os nomes das colunas
+
     with open(r'C:\\Users\\thiag\\Desktop\\new_data\\HEADERS_test.txt', 'w') as fp:
         fp.write("\n".join(str(item) for item in cols))
         fp.close()
@@ -115,7 +154,6 @@ def get_table_len():
             
 total_len = get_table_len()
 
-# Esta função calcula quantas linhas serão lidas por cada computação paralela
 def return_p(total_processes,total_len):
     if (total_len % total_processes)== 0:
         p=int(total_len / total_processes)
@@ -128,12 +166,8 @@ def return_p(total_processes,total_len):
     return p1,p
 
 
-
-
 p1, p = return_p(10, total_len)
 
-# Esta função cria duas FIFO com 10 elementos cada: uma para o elemento inicial e outra para o elemento final
-# de cada linha que deverá ser lida da tabela de resultados da mega sena
 def gen_from_to(p1, p, total_processes):
     from_p = queue.Queue(total_processes)
     to_p = queue.Queue(total_processes)
@@ -153,87 +187,136 @@ def gen_from_to(p1, p, total_processes):
 
 from_p, to_p = gen_from_to(p1, p, 10)
 
-# Esta função acessa a tabela de resultados e recupera os nomes das colunas e os dados de cada linha
 def get_megasena_results(f_p,t_p, sufix):
 
     start_time=datetime.now().strftime("%H:%M:%S")
     print('start time:',start_time)
     load_failed=False
-    tentativas=10
-    # Configura o webdriver.chrome em uma variável chamada browser
+    tentativas=15
 
     try:
         browser = webdriver.Chrome("C:\Program Files (x86)\chromedriver.exe")
     except Exception as e:
         print(e)
         
-    browser.implicitly_wait(10)
-
-
-    # Acessa a página da megasena
 
     MEGASENA_URL = "https://loterias.caixa.gov.br/Paginas/Mega-Sena.aspx" 
     browser.get(MEGASENA_URL)
 
-    # Maximiza a página atualmente aberta
-    browser.maximize_window()
-
-    timeout = 45 
+    timeout = 10 
     try:
         element_present = EC.presence_of_element_located((By.ID, 'PlaceHolderMain_g_8948ce2c_ab1a_4e3c_95ec_8eb3565a1df1'))
         WebDriverWait(browser, timeout).until(element_present)
     except :
-        print("Timed out waiting for page to load - 1")
-        time.sleep(10)
+        print("Timed out na tentativa de carregamento da página principal em get_table_len")
+        print('Processo:',sufix)
+        time.sleep(2)
+        load_failed=True
 
-    # Efetua clique em "aceitar" no banner aberto sobre a página principal com objetivo de fechá-lo e permitir
-    # o controle remoto da página pelo selenium
+    while load_failed==True and tentativas > 0 :
+        browser.refresh()
+        time.sleep(40)
+        timeout = 40
+        try:
+            element_present = EC.presence_of_element_located((By.ID, 'PlaceHolderMain_g_8948ce2c_ab1a_4e3c_95ec_8eb3565a1df1'))
+            WebDriverWait(browser, timeout).until(element_present)
+            
+        except:
+            if tentativas > 0:
+                tentativas = tentativas - 1
+                print('Tentando novamente...')
+
+            else:
+                print('Todas as tentativas de carregamento da pagina falharam...')
+                exit()
+        else:
+            load_failed=False
+
+    tentativas=15
+
+    browser.maximize_window()
+
+
+    timeout = 10 
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[2]/button[3]'))
+        WebDriverWait(browser, timeout).until(element_present)
+    except :
+        print("Botão de aceite não localizado na página principal.")
+        time.sleep(2)
+        load_failed=True
+
+    while load_failed==True and tentativas > 0 :
+        browser.refresh()
+        time.sleep(40)
+        timeout = 40
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[2]/button[3]'))
+            WebDriverWait(browser, timeout).until(element_present)
+            
+        except:
+            if tentativas > 0:
+                tentativas = tentativas - 1
+                print('Tentando novamente...')
+
+            else:
+                print('Todas as tentativas falharam...')
+                exit()
+        else:
+            load_failed=False
+
+    tentativas=15
    
     clicar=browser.find_element(By.XPATH,'/html/body/div[4]/div/div[2]/button[3]') 
-    time.sleep(5)
+    time.sleep(2)
     clicar.click()
 
 
 
-    # Efetua o clique no link de resultados de jogos anteriores da megasena
     RESULTADOS_URL=browser.find_element(By.CSS_SELECTOR, "#resultados > div > ul > li > a")
-    time.sleep(3)
+    time.sleep(2)
     RESULTADOS_URL.click()
-    time.sleep(5)
-    # Muda a target screen para a página resultados recentemente aberta  
+    time.sleep(2)
+
     browser.switch_to.window(browser.window_handles[1])
 
-    timeout = 15
+    timeout = 10
     try:
-        element_present = EC.presence_of_element_located((By.ID, 'ctl50_g_cf05b8d5_fd75_46b5_bdfa_a623e654362c'))
+        element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="ctl50_g_cf05b8d5_fd75_46b5_bdfa_a623e654362c"]/div/div/table/thead/tr/th[1]/small'))
         WebDriverWait(browser, timeout).until(element_present)
     except:
-        print("Timed out waiting for page to load - 2 in process"+str(sufix))
+        print('Link dos resultados anteriores não encontrado.')
         load_failed=True
+        browser.refresh()
 
     while load_failed==True and tentativas > 0 :
-
-        timeout = 15
+        print('while')
+        browser.refresh()
+        time.sleep(40)
+        timeout = 40
         try:
-            element_present = EC.presence_of_element_located((By.ID, 'ctl50_g_cf05b8d5_fd75_46b5_bdfa_a623e654362c'))
+            element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="ctl50_g_cf05b8d5_fd75_46b5_bdfa_a623e654362c"]/div/div/table/thead/tr/th[1]/small'))
             WebDriverWait(browser, timeout).until(element_present)
             
         except:
-            print("Timed out waiting for page to load - 2 in process"+str(sufix))
-            browser.refresh()
-            tentativas = tentativas - 1
-        finally:
+            if tentativas > 0:
+                tentativas = tentativas - 1
+                print('Tentando novamente...')
+
+            else:
+                print('Todas as tentativas falharam...')
+                exit()
+
+        else:
             load_failed=False
             
-    # De posse do número total de linhas, efetua-se a raspagem dos dados de todos os sorteios já realizados
-    # Cria data frame com os dados obtidos
     data_el = []
     for i in range(f_p, t_p + 1):
         for z in range(1, 22):
             data_el.append(browser.find_element(
                 By.XPATH, '//*[@id="ctl50_g_cf05b8d5_fd75_46b5_bdfa_a623e654362c"]/div/div/table/tbody['+str(i)+']/tr/td['+str(z)+']').text)
 
-    time.sleep(5)
+    time.sleep(2)
     with open('C:\\Users\\thiag\\Desktop\\new_data\\megasena_data' + str(sufix) + '.csv', 'w') as file:
         writer = csv.writer(file)
         writer.writerow(data_el)
